ceb652b97c9318abe55e68501ecd829ddb03ed87
Ariel0203 <cocimanoariel@hotmail.com>
2024-02-14 13:45:57 -0300

    Cambie los botones en HTMAL y CSS

---
+++ app.js
@@ -0,0 +1,12 @@
+let textoUsuario = document.getElementById('textoIngresado').value;
+console.log("Texto ingresado: " + textoUsuario);
+
+
+function encriptacion(){
+  
+}
+
+function desencriptacion(){
+
+}
+
